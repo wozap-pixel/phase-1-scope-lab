0cb404926f68ad6ee685d586ddf21e26240b6819
peter <mutisyapeter336@gmail.com>
2024-10-22 07:18:25 +0300

    added a new file

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+// customerManagement.js
+
+// Declare customerName in the global scope
+window.customerName = 'bob';
+
+// Function to modify customerName to uppercase
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+}
+
+// Function to set bestCustomer
+function setBestCustomer() {
+    window.bestCustomer = 'not bob';
+}
+
+// Function to overwrite bestCustomer
+function overwriteBestCustomer(name) {
+    window.bestCustomer = name;
+}
+
+// Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'someone';
+
+// Function that attempts to change leastFavoriteCustomer
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'another person'; // This will throw an error
+}
